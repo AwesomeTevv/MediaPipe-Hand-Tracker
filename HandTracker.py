60540e3cf6dc4bfcb4a8deab6c12689abad13a27
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-01-27 14:02:57 +0200

    Added fingertip tracking

--- HandTracker.py
+++ HandTracker.py
@@ -23,10 +23,17 @@ while True:
     
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                
+                if id == 4 or id == 8 or id == 12 or id == 16 or id == 20:
+                    cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+                
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
     
     cTime = time.time()
-    fps = 1/(cTime - pTime)
+    fps = 1 / (cTime - pTime)
     pTime = cTime
     
     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
