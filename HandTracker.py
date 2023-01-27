87a04eeb9012e54af2a7e9a59fd30c0083d096ba
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-01-27 13:44:01 +0200

    Added fps

--- HandTracker.py
+++ HandTracker.py
@@ -9,6 +9,9 @@ hands = mpHands.Hands(False)
 
 mpDraw = mp.solutions.drawing_utils
 
+pTime = 0
+cTime = 0
+
 while True:
     success, img = cap.read()
     
@@ -16,11 +19,17 @@ while True:
     
     results = hands.process(imgRGB)
     
-    print(results.multi_hand_landmarks)
+    # print(results.multi_hand_landmarks)
     
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+    
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
     
     cv2.imshow("Image", img)
     cv2.waitKey(1)
